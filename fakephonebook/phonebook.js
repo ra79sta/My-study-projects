babed8060df89be1eab90cae51f38c5f0e25a847
ra79sta <staca_r@hotmail.com>
2018-07-18 16:04:02 +0200

    Fake phonebook FrontEnd

--- fakephonebook/phonebook.js
+++ fakephonebook/phonebook.js
@@ -35,7 +35,7 @@ $(document).ready(function () {
                 }
             });
             resetData();
-            del();
+           // del();
         } else {
             alert("Enter data!!!")
         }
