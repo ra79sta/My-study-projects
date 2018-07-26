5c064ac32270eec6dbc5c9a1ba7c8ab4ad5aa7fc
ra79sta <staca_r@hotmail.com>
2018-07-26 17:28:50 +0200

    VueTask1

---
+++ vuetask1/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <VueTask1/>
+  </div>
+</template>
+
+<script>
+import VueTask1 from './components/VueTask1'
+
+export default {
+  name: 'App',
+  components: {
+    VueTask1
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vuetask1/src/components/VueTask1.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="task">
+    <h1>{{ msg }}</h1>
+    <div v-if="imageData.length > 0">
+      <img class="preview" :src="imageData">
+    </div>
+    <div>Upload an image:
+      <input type="file" @change="previewImage" accept="image/*"> 
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VueTask1',
+  data () {
+    return {
+      msg: 'Vue task!',
+      imageData: ""
+      }
+  },
+  
+  methods: {
+      previewImage (event) {
+        var pics = event.target;
+        if (pics.files && pics.files[0]) {
+          var reader = new FileReader();
+          reader.onload = (e) => {
+            this.imageData = e.target.result;
+          }
+          reader.readAsDataURL(pics.files[0]);
+        }
+      }
+    }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+h1 {
+  font-weight: normal;
+}
+img.preview {
+  width: 200px;
+  background-color: white;
+  border: 1px solid #ddd;
+  padding: 5px;
+}
+</style>
