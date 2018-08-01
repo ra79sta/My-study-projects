2fd218737dceeaccc5d9b8d294e153a9b6c3003d
ra79sta <staca_r@hotmail.com>
2018-08-01 12:43:14 +0200

    Make array of 7 integers of number larger the 100 / (sum of array = number)

--- zadatak1/task.js
+++ zadatak1/task.js
@@ -1,33 +1,41 @@
 function makeArray(num) {
-	var numero = num;
-	var numOfElements = 7;
-	var arr = [];
-	var arrSum = 0;
-	var ints;
-	var lastInt;
-	if (numero > 100) {
+    var numero = num;
+    var numOfElements = 7;
+    var arr = [];
+    var arrSum = 0;
+    var ints;
+    var lastInt;
+    if (numero > 100) {
 
-		for (var i = 0; i < numOfElements - 1; i++) {
+        for (var i = 0; i < numOfElements - 1; i++) {
 
-			ints = Math.round(Math.random() * (numero - arrSum) / 2) + 1;
-			if (arr.indexOf(ints) == -1) {
-				arr.push(ints);
-			} else {
-				i--;
-			}
-			arrSum = arr.reduce((a, b) => a + b, 0);
+            ints = Math.round(Math.random() * (numero - arrSum) / 2) + 1;
+            if (arr.indexOf(ints) == -1) {
+                arr.push(ints);
+            } else {
+                i--;
+            }
+            arrSum = arr.reduce((a, b) => a + b, 0);
 
-		}
+        }
 
-		lastInt = numero - arrSum;
-		arr.push(lastInt);
-		arr.sort((a, b) => b - a);
+        lastInt = numero - arrSum;
+        arr.push(lastInt);
+        arr.sort((a, b) => b - a);
 
 
-		console.log(arr);
-	} else {
-		console.log("Enter Number Larger then 100")
-	}
+        console.log(arr);
+    } else {
+        console.log("Enter Number Larger then 100")
+    }
 
+    function check(arr) {
+        return arr.every(x => arr.indexOf(x) === arr.lastIndexOf(x));
+    }
+    if (check(arr)) {
+        console.log(arr)
+    } else {
+        makeArray(num);
+    }
 }
 makeArray(101);
