4a5172d7466d7777a940c83109eb5a1b96fa97fc
ra79sta <staca_r@hotmail.com>
2018-08-14 14:16:08 +0200

    Make array of 7 integers of number larger the 100 / (sum of array = number)

--- zadatak1/task.js
+++ zadatak1/task.js
@@ -10,6 +10,8 @@ function makeArray(num) {
     var arrSum = 0;
     var ints;
     var lastInt;
+    var min;
+    var secondMin;
     if (numero > 100) {
 
         for (var i = 0; i < numOfElements - 1; i++) {
@@ -20,12 +22,22 @@ function makeArray(num) {
 
         lastInt = numero - arrSum;
         arr.push(lastInt);
-        arr.sort((a, b) => b - a);
+        //arr.sort((a, b) => b - a);
+
+        //smallest two number of array pushed to the end of array
+        min = Math.min.apply(null, arr);
+        secondMin = arr.reduce((pre, cur) => (cur < pre && cur !== min) ? cur : pre, Infinity);
+        arr.pop(min);
+        arr.pop(secondMin);
+        arr.push(min);
+        arr.push(secondMin);
+
+
 
     } else {
         console.log("Enter Number Larger then 100")
     }
-
+    // check for duplicates(if there are no duplicates two numbers has to be smallest)
     function check(arr) {
         return arr.every(x => arr.indexOf(x) === arr.lastIndexOf(x));
     }
